9a5b7c853221e96a14bce184f2c4dcf5dd35b584
Keishi Asai <isheik808@gmail.com>
2024-07-31 07:22:58 -0700

    fix

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via-kcustom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via-kcustom/keymap.c
@@ -88,10 +88,10 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             rgblight_sethsv(HSV_SPRINGGREEN);
             break;
         case 2:
-            rgblight_sethsv(HSV_MAGENTA);
+            rgblight_sethsv(HSV_BLUE);
             break;
         case 3:
-            rgblight_sethsv(HSV_BLUE);
+            rgblight_sethsv(HSV_GREEN);
             break;
         case 4:
             rgblight_sethsv(HSV_CYAN);

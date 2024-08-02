76f873a5114f57c3f829fbc3850a1f654d92fdf8
Keishi Asai <isheik808@gmail.com>
2024-08-01 23:34:18 -0700

    fix

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via-kcustom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via-kcustom/keymap.c
@@ -82,19 +82,18 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     uint8_t layer = biton32(state);
     switch (layer) {
         case 0:
-            rgblight_sethsv(HSV_ORANGE);
             break;
         case 1:
             rgblight_sethsv(HSV_SPRINGGREEN);
             break;
         case 2:
-            rgblight_sethsv(HSV_GOLDENROD);
+            rgblight_sethsv(HSV_ORANGE);
             break;
         case 3:
-            rgblight_sethsv(HSV_CHARTREUSE);
+            rgblight_sethsv(HSV_CYAN);
             break;
         case 4:
-            rgblight_sethsv(HSV_CYAN);
+            rgblight_sethsv(HSV_CHARTREUSE);
             break;
     }
 
